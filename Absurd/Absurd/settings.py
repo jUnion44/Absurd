7adaafc89308992bfc374dc07a3d6285ee10030a
BuildTools <jUnion444@gmail.com>
2020-12-04 16:57:49 +0000

    Admin update

--- Absurd/Absurd/settings.py
+++ Absurd/Absurd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4ea$epbbvc1y8&so^z(0#gf7sx=94ua==$lllh#_-ez=dn#r_6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
--- Absurd/core/admin.py
+++ Absurd/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Prompt)
+admin.site.register(Choice)
