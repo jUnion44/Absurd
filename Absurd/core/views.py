bb2c853353e98b498beac3ab938083f8a8658d3f
BuildTools <jUnion444@gmail.com>
2020-12-09 16:39:57 +0000

    random choice

--- Absurd/core/views.py
+++ Absurd/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from .models import *
+import random
 
 STARTER_ID=3
+
+#0=random 1=precoded
+CHOICE_SETTING=0
 
 # Create your views here.
 def index(request):
@@ -11,7 +15,13 @@ def index(request):
 def loadPrompt(request,promptid):
     p = get_object_or_404(Prompt,pk=promptid)
     responsedict = {"prompt":p.text,"choices":[]}
+    prompts = list(Prompt.objects.order_by('id').values())
     for c in p.befores.all():
-        responsedict["choices"].append([c.text,c.promptNext.id])
+        if c.random:
+            responsedict["choices"].append([c.text,random.choice(prompts)["id"]])
+        else:
+            responsedict["choices"].append([c.text,c.promptNext.id])
+        
+        
     response = JsonResponse(responsedict)
     return response
